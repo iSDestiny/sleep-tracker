b08190b904bfcb7bb92ff7e20e027e3e2ed806d5
pixeltopic <32804347+pixeltopic@users.noreply.github.com>
2019-11-24 19:42:58 -0800

    Added overnight sleep logging

--- sleeptracker/src/app/overnightsleep/overnightsleep.page.ts
+++ sleeptracker/src/app/overnightsleep/overnightsleep.page.ts
@@ -9,24 +9,50 @@ import { SleepService } from '../services/sleep.service';
 })
 export class OvernightsleepPage implements OnInit {
 
-  constructor(sleepService: SleepService) { }
+  public sleepDate;
+  public sleepTime;
+
+  public wakeDate;
+  public wakeTime;
+
+  constructor(private sleepService: SleepService) { }
 
   ngOnInit() {
   }
 
-  getCurrentTime() {
-    console.log(new Date());
+  handleSleepTime(): void {
+    if (!this.sleepDate || !this.sleepTime) {
+      return
+    }
+    let sd: Date = new Date(this.sleepDate);
+    let st: Date = new Date(this.sleepTime);
+    console.log(`The date of sleep is: ${sd.getMonth()}-${sd.getDate()}-${sd.getFullYear()}`);
+    console.log(`The time of sleep is: ${st.getHours()}:${String(st.getMinutes()).length === 1 ? `0${st.getMinutes()}` : st.getMinutes()}:00`);
     // new OvernightSleepData()
   }
 
-  updateMyDate($event) {
-    const day: number = $event.day.value;
-    const month: number = $event.month.value;
-    const year: number = $event.year.value;
+  handleWakeTime(): void {
+    if (!this.sleepDate || !this.sleepTime || !this.wakeDate || !this.wakeTime) {
+      return
+    }
+    let wd: Date = new Date(this.wakeDate);
+    let wt: Date = new Date(this.wakeTime);
+    let sd: Date = new Date(this.sleepDate);
+    let st: Date = new Date(this.sleepTime);
+    // console.log(`The date of sleep is: ${sd.getMonth()}-${sd.getDate()}-${sd.getFullYear()}`);
+    // console.log(`The time of sleep is: ${st.getHours()}:${String(st.getMinutes()).length === 1 ? `0${st.getMinutes()}` : st.getMinutes()}:00`);
+    // console.log(`The date of wake is: ${wd.getMonth()}-${wd.getDate()}-${wd.getFullYear()}`);
+    // console.log(`The time of wake is: ${wt.getHours()}:${String(wt.getMinutes()).length === 1 ? `0${wt.getMinutes()}` : wt.getMinutes()}:00`);
 
-    console.log(month, " ", day, ", ", year);
-    // console.log($event);
+    const sleepDate: Date = new Date(sd.getFullYear(), sd.getMonth(), sd.getDate(), st.getHours(), st.getMinutes())
+    const wakeDate: Date = new Date(wd.getFullYear(), wd.getMonth(), wd.getDate(), wt.getHours(), wt.getMinutes())
+
+    const overnightSleepData = new OvernightSleepData(sleepDate, wakeDate);
+    console.log(overnightSleepData.summaryString());
+
+    this.sleepService.logOvernightData(overnightSleepData);
+
+    console.log("All overnight data:", SleepService.AllOvernightData);
+
   }
-  
-
 }
