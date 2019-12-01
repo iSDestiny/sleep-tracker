1545f1b5e06bfdcfc73d88cf333eb8b09d348574
Jason <jbugallo@uci.edu>
2019-11-30 20:47:19 -0800

    implemented data storage functionality

--- sleeptracker/src/app/services/sleep.service.ts
+++ sleeptracker/src/app/services/sleep.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { SleepData } from '../data/sleep-data';
 import { OvernightSleepData } from '../data/overnight-sleep-data';
 import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
+import {Storage} from "@ionic/storage";
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +13,43 @@ export class SleepService {
 	public static AllOvernightData:OvernightSleepData[] = [];
 	public static AllSleepinessData:StanfordSleepinessData[] = [];
 
-  constructor() {
-    console.log("this got recreated")
-  	if(SleepService.LoadDefaultData) {
-      this.addDefaultData();
-  		SleepService.LoadDefaultData = false;
-  	}
+  constructor(private storage: Storage) {
+    this.storage.get('sleepData').then((data) => {
+      if(data) {
+        data.forEach((item) => {
+            if(item.hasOwnProperty('loggedValue')) {
+              SleepService.AllSleepData.push(new StanfordSleepinessData(item.loggedValue, item.loggedAt));
+            }else {
+              SleepService.AllSleepData.push(new OvernightSleepData(item.sleepStart, item.sleepEnd));
+            }
+        })
+      }else {
+        return this.storage.set('sleepData', []);
+      }
+    });
+    this.storage.get('overnightData').then((data) => {
+      if(data) {
+        data.forEach((item) => {
+          SleepService.AllOvernightData.push(new OvernightSleepData(item.sleepStart, item.sleepEnd));
+        })
+      }else {
+        return this.storage.set('overnightData', []);
+      }
+    });
+    this.storage.get('sleepinessData').then((data) => {
+      if(data) {
+        data.forEach((item) => {
+          SleepService.AllSleepinessData.push(new StanfordSleepinessData(item.loggedValue, item.loggedAt));
+        })
+      }else {
+        return this.storage.set('sleepinessData', []);
+      }
+    });
+    console.log("this got recreated");
+  	// if(SleepService.LoadDefaultData) {
+      // this.addDefaultData();
+      // SleepService.LoadDefaultData = false;
+  	// }
   }
 
   private addDefaultData() {
@@ -29,10 +61,46 @@ export class SleepService {
   public logOvernightData(sleepData:OvernightSleepData) {
   	SleepService.AllSleepData.push(sleepData);
   	SleepService.AllOvernightData.push(sleepData);
+    this.storage.get('sleepData').then((data) => {
+      if(data) {
+        data.push(sleepData);
+          return this.storage.set('sleepData', data);
+        }else {
+          return this.storage.set('sleepData', [sleepData]);
+        }
+    });
+
+    this.storage.get('overnightData').then((data) => {
+      if(data) {
+        data.push(sleepData);
+          return this.storage.set('overnightData', data);
+        }else {
+          return this.storage.set('overnightData', [sleepData]);
+        }
+    });
   }
 
   public logSleepinessData(sleepData:StanfordSleepinessData) {
   	SleepService.AllSleepData.push(sleepData);
   	SleepService.AllSleepinessData.push(sleepData);
+
+  	this.storage.get('sleepData').then((data) => {
+      if(data) {
+        data.push(sleepData);
+          return this.storage.set('sleepData', data);
+      }else {
+          return this.storage.set('sleepData', [sleepData]);
+        }
+    });
+
+    this.storage.get('sleepinessData').then((data) => {
+      if(data) {
+          data.push(sleepData);
+          return this.storage.set('sleepinessData', data);
+      }else {
+          return this.storage.set('sleepinessData', [sleepData]);
+      }
+  });
+
   }
 }
